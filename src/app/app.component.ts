38b0367c78598dd7bcae7ef18a4ebfed80c0b963
Shubham Manchalkar <shubham.manchalkar97@gmail.com>
2021-08-08 18:57:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'book-mark';
+export class AppComponent implements OnInit{
+ 
+  rows:any;
+  public ColumnMode = ColumnMode;
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.http.get("https://bookmarkapi.azurewebsites.net/api/BookMark").subscribe(
+      (res)=>{
+        console.log(res);
+        this.rows=res;
+      }
+    );
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from "@angular/common/http"
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxDatatableModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
